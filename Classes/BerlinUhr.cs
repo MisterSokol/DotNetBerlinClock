de5db2b05e1564c8b3333bcf09b5bf544ccfcac1
Krzysztof Sokolowski <sokolowski.krzysztof.1993@gmail.com>
2020-05-17 09:46:19 +0200

    Add unimplemented BerlinUhr class

---
+++ Classes/BerlinUhr.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BerlinClock.Classes
+{
+	public class BerlinUhr : IBerlinUhr
+	{
+		public BerlinUhr(IBerlinUhrPrintStrategy printStrategy)
+		{
+		}
+
+		public bool SecondDot => throw new NotImplementedException();
+
+		public IEnumerable<bool> FiveFullHoursRow => throw new NotImplementedException();
+
+		public IEnumerable<bool> OneFullHoursRow => throw new NotImplementedException();
+
+		public IEnumerable<bool> FiveFullMinutesRow => throw new NotImplementedException();
+
+		public IEnumerable<bool> OneFullMinutesRow => throw new NotImplementedException();
+
+		public string Print()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void SetTime(TimeSpan time)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
