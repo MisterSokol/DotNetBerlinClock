174c1711de86b37afeb041546aed9ef375cb3e71
Krzysztof Sokolowski <sokolowski.krzysztof.1993@gmail.com>
2020-05-17 09:44:34 +0200

    Add IBerlinUhr interface

---
+++ Classes/IBerlinUhr.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace BerlinClock.Classes
+{
+	public interface IBerlinUhr
+	{
+		bool SecondDot { get; }
+		IEnumerable<bool> FiveFullHoursRow { get; }
+		IEnumerable<bool> OneFullHoursRow { get; }
+		IEnumerable<bool> FiveFullMinutesRow { get; }
+		IEnumerable<bool> OneFullMinutesRow { get; }
+
+		void SetTime(TimeSpan time);
+		string Print();
+	}
+}
