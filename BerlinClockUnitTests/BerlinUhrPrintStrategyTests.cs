0b8fb4f6900b11ab89031dc5e88c97ecba519438
Krzysztof Sokolowski <sokolowski.krzysztof.1993@gmail.com>
2020-05-18 18:37:10 +0200

    Add BerlinUhrPrintStrategy with tests

---
+++ BerlinClockUnitTests/BerlinUhrPrintStrategyTests.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using BerlinClock.Classes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace BerlinClockUnitTests
+{
+	[TestClass]
+	public class BerlinUhrPrintStrategyTests
+	{
+		private Mock<IBerlinUhr> berlinUhrMock;
+
+		BerlinUhrPrintStrategy sut;
+		
+		[TestInitialize]
+		public void TestInitialize()
+		{
+			this.sut = new BerlinUhrPrintStrategy();
+
+			this.berlinUhrMock = new Mock<IBerlinUhr>();
+
+			this.berlinUhrMock
+				.Setup(uhr => uhr.OneFullMinutesRow)
+				.Returns(new bool[4]);
+			this.berlinUhrMock
+				.Setup(uhr => uhr.FiveFullMinutesRow)
+				.Returns(new bool[11]);
+			this.berlinUhrMock
+				.Setup(uhr => uhr.OneFullHoursRow)
+				.Returns(new bool[4]);
+			this.berlinUhrMock
+				.Setup(uhr => uhr.FiveFullHoursRow)
+				.Returns(new bool[4]);
+		}
+
+		[TestMethod]
+		public void Should_AllDotsBeOff_When_EveryDotValueIsFalse()
+		{
+			var expected = 
+				"O\r\n"+
+				"OOOO\r\n" +
+				"OOOO\r\n" +
+				"OOOOOOOOOOO\r\n" +
+				"OOOO";
+
+			var actual = sut.Print(this.berlinUhrMock.Object);
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void Should_SecondDotBeY_When_SecondDotValueIsTrue()
+		{
+			var expected =
+				"Y\r\n" +
+				"OOOO\r\n" +
+				"OOOO\r\n" +
+				"OOOOOOOOOOO\r\n" +
+				"OOOO";
+
+			this.berlinUhrMock
+				.Setup(uhr => uhr.SecondDot)
+				.Returns(true);
+
+			var actual = sut.Print(this.berlinUhrMock.Object);
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void Should_AllFullFiveHourDotsBeR_When_AllFullFiveHourValuesAreTrue()
+		{
+			var expected =
+				"O\r\n" +
+				"RRRR\r\n" +
+				"OOOO\r\n" +
+				"OOOOOOOOOOO\r\n" +
+				"OOOO";
+
+			this.berlinUhrMock
+				.Setup(uhr => uhr.FiveFullHoursRow)
+				.Returns(new[] { true, true, true, true});
+
+			var actual = sut.Print(this.berlinUhrMock.Object);
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void Should_AllFullOneHourDotsBeR_When_AllFullOneHourValuesAreTrue()
+		{
+			var expected =
+				"O\r\n" +
+				"OOOO\r\n" +
+				"RRRR\r\n" +
+				"OOOOOOOOOOO\r\n" +
+				"OOOO";
+
+			this.berlinUhrMock
+				.Setup(uhr => uhr.OneFullHoursRow)
+				.Returns(new[] { true, true, true, true });
+
+			var actual = sut.Print(this.berlinUhrMock.Object);
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void Should_AllQuarterMinutesBeRAndRestY_When_AllFullFiveMinutesaluesAreTrue()
+		{
+			var expected =
+				"O\r\n" +
+				"OOOO\r\n" +
+				"OOOO\r\n" +
+				"YYRYYRYYRYY\r\n" +
+				"OOOO";
+
+			this.berlinUhrMock
+				.Setup(uhr => uhr.FiveFullMinutesRow)
+				.Returns(new[] { true, true, true, true, true, true, true, true, true, true, true });
+
+			var actual = sut.Print(this.berlinUhrMock.Object);
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void Should_AllFullOneMinutesDotsBeY_When_AllFullOneMinutesValuesAreTrue()
+		{
+			var expected =
+				"O\r\n" +
+				"OOOO\r\n" +
+				"OOOO\r\n" +
+				"OOOOOOOOOOO\r\n" +
+				"YYYY";
+
+			this.berlinUhrMock
+				.Setup(uhr => uhr.OneFullMinutesRow)
+				.Returns(new[] { true, true, true, true });
+
+			var actual = sut.Print(this.berlinUhrMock.Object);
+
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
---
+++ Classes/BerlinUhrPrintStrategy.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace BerlinClock.Classes
+{
+	public class BerlinUhrPrintStrategy : IBerlinUhrPrintStrategy
+	{
+		private const string offValue = "O";
+		private const string hourValue = "R";
+		private const string minuteValue = "Y";
+		private const string quarterValue = "R";
+		private const string secondValue = "Y";
+
+		public string Print(IBerlinUhr berlinUhr)
+		{
+			var stringBuilder = new StringBuilder();
+
+			stringBuilder.AppendLine(this.GetSecondDotString(berlinUhr));
+			stringBuilder.AppendLine(this.GetFiveFullHoursRowString(berlinUhr));
+			stringBuilder.AppendLine(this.GetOneFullHoursRowString(berlinUhr)); 
+			stringBuilder.AppendLine(this.GetFiveFullMinutesRowString(berlinUhr)); 
+			stringBuilder.Append(this.GetOneFullMinutesRowString(berlinUhr));
+
+			return stringBuilder.ToString();
+		}
+
+		private string GetOneFullMinutesRowString(IBerlinUhr berlinUhr)
+		{
+			var stringBuilder = new StringBuilder();
+
+			foreach (var fullOne in berlinUhr.OneFullMinutesRow)
+			{
+				stringBuilder.Append(fullOne ? minuteValue : offValue);
+			}
+
+			return stringBuilder.ToString();
+		}
+
+		private string GetFiveFullMinutesRowString(IBerlinUhr berlinUhr)
+		{
+			var stringBuilder = new StringBuilder();
+			var fullFiveMinutes = berlinUhr.FiveFullMinutesRow.ToList();
+
+			for (int i = 1; i <= fullFiveMinutes.Count; i++)
+			{
+				var isQuarter = i % 3 == 0;
+				var onValue = isQuarter ? quarterValue : minuteValue;
+				stringBuilder.Append(fullFiveMinutes[i-1] ? onValue : offValue);
+			}
+
+			return stringBuilder.ToString();
+		}
+
+		private string GetOneFullHoursRowString(IBerlinUhr berlinUhr)
+		{
+			var stringBuilder = new StringBuilder();
+
+			foreach (var fullOne in berlinUhr.OneFullHoursRow)
+			{
+				stringBuilder.Append(fullOne ? hourValue : offValue);
+			}
+
+			return stringBuilder.ToString();
+		}
+
+		private string GetFiveFullHoursRowString(IBerlinUhr berlinUhr)
+		{
+			var stringBuilder = new StringBuilder();
+
+			foreach (var fullFive in berlinUhr.FiveFullHoursRow)
+			{
+				stringBuilder.Append(fullFive ? hourValue : offValue);
+			}
+
+			return stringBuilder.ToString();
+		}
+
+		private string GetSecondDotString(IBerlinUhr berlinUhr)
+		{
+			return berlinUhr.SecondDot ? secondValue : offValue;
+		}
+	}
+}
