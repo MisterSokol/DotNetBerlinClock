38526af3fe579960bcdd6f188cb708f6543d3044
Krzysztof Sokolowski <sokolowski.krzysztof.1993@gmail.com>
2020-05-18 18:38:13 +0200

    Implement TimeConverter

--- Classes/TimeConverter.cs
+++ Classes/TimeConverter.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using BerlinClock.Classes;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Net.Configuration;
 using System.Text;
 
 namespace BerlinClock
@@ -9,7 +12,22 @@ namespace BerlinClock
     {
         public string convertTime(string aTime)
         {
-            throw new NotImplementedException();
+            var time = this.GetTime(aTime);
+            var berlinUhr = new BerlinUhr(new BerlinUhrPrintStrategy());
+
+            berlinUhr.SetTime(time);
+
+            return berlinUhr.Print();
+        }
+
+        private TimeSpan GetTime(string aTime)
+        {
+            var timeParts = aTime
+                .Split(':')
+                .Select(tpart => int.Parse(tpart))
+                .ToArray();
+
+            return new TimeSpan(timeParts[0], timeParts[1], timeParts[2]);
         }
     }
 }
